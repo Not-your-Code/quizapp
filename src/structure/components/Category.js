3bd673737d59df7c419fade324f5bb58d3548e48
Not-Your-Code <vineetagarwal.now@gmail.com>
2023-03-11 02:12:25 +0530

    done with category.js logic

--- src/structure/components/Category.js
+++ src/structure/components/Category.js
@@ -2,6 +2,19 @@ import React, { useEffect, useState } from 'react'
 
 export default function Category() {
     const [categories, setCategories] = useState({});
+    const [userCat, setUsercat] = useState('History')
+    const [userMode, setUserMode] = useState('Easy')
+    const [userLimit, setUserLimit] = useState(1)
+    const mode = ["easy ", "medium", "hard"];
+    const limit = [];
+    const [fetched, setFetched] = useState({})
+
+    for (let i = 1; i <= 20; i++) {
+        limit.push(i);
+    }
+
+
+
     useEffect(() => {
         const url = 'https://the-trivia-api.com/api/categories'
 
@@ -16,12 +29,8 @@ export default function Category() {
             }
         };
         fetchData();
-    }, [1])
+    }, [])
 
-    useEffect(() => {
-        console.log(categories)
-
-    }, [categories])
 
 
     const objKeys = [];
@@ -29,17 +38,44 @@ export default function Category() {
         objKeys.push(key);
     }
 
-    const handleSubmit = () => {
+    const handleChangeCat = (e) => setUsercat(e.target.value)
+    const handleChangeMode = (e) => setUserMode(e.target.value)
+    const handleChangeLimit = (e) => setUserLimit(e.target.value)
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const FetchUrl = `https://the-trivia-api.com/api/questions?categories=${userCat}&limit=${userLimit}&difficulty=${userMode}`
+        console.log(FetchUrl)
+        fetchData_user(FetchUrl)
 
     }
+
+    const fetchData_user = async (url) => {
+        try {
+            const response = await fetch(url);
+            const json = await response.json();
+            setFetched(json)
+
+        } catch (error) {
+            console.log("error", error);
+        }
+    };
 
     return (
 
         <div className='category'>
-            <form method='get' onSubmit={handleSubmit()}>
+            <form method='get' onSubmit={handleSubmit} >
                 <label >Category </label>
-                <select id="cars" name="cars">
-                    {objKeys.map((key, i) => (<option value={key}>{key}</option>))}
+
+                <select value={userCat} onChange={handleChangeCat}>
+                    {objKeys.map(key => (<option value={key}>{key}</option>))}
+                </select>
+                <select value={userMode} onChange={handleChangeMode}>
+                    {mode.map(key => (<option value={key}>{key}</option>))}
+                </select>
+                <select value={userLimit} onChange={handleChangeLimit}>
+                    {limit.map(key => (<option value={key}>{key}</option>))}
                 </select>
                 <button type='submit'>submit</button>
             </form>
